63488b3cd86f1b40605daf9b1cd76c2504135e09
pixzel <syztek@gmail.com>
2013-09-18 18:48:31 +0200

    MARKENSGATE.NO FORSLAG 1 v.01
    - Forslag 1
    
    av
    Pixzel

---
+++ minsql.sql
@@ -0,0 +1,2 @@
+CREATE TABLE `counter` ( `counter` INT( 20 ) NOT NULL );
+ INSERT INTO counter VALUES (0); 
---
+++ welcome.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+&lt;html&gt;<br />
+&lt;body&gt;<br />
+<br />
+Welcome &lt;?php echo $_POST[&quot;fname&quot;]; ?&gt;!&lt;br&gt;<br />
+You are &lt;?php echo $_POST[&quot;age&quot;]; ?&gt; years old.<br />
+<br />
+&lt;/body&gt;<br />
+&lt;/html&gt;
+</body>
+</html>
