e451f11bbe4bd48d3cf8fff4687cf59fe40a0efd
Behzad2256 <behzad@hotmail.no>
2013-09-26 15:00:24 +0200

    fix
    fix

--- www.markensgate.no/registrering.php
+++ www.markensgate.no/registrering.php
@@ -2,7 +2,69 @@
 <head>
 <title>
 </title>
-
+<script type="text/javascript">
+function validateForm()
+{
+var a=document.forms["reg"]["fname"].value;
+var b=document.forms["reg"]["lname"].value;
+var c=document.forms["reg"]["mname"].value;
+var d=document.forms["reg"]["address"].value;
+var e=document.forms["reg"]["contact"].value;
+var f=document.forms["reg"]["username"].value;
+var g=document.forms["reg"]["password"].value;
+var h=document.forms["reg"]["gpassword"].value;
+/*if ((a==null || a=="") && (b==null || b=="") && (c==null || c=="") && (d==null || d=="") && (e==null || e=="") && (f==null || f==""))
+  {
+  alert("All Field must be filled out");
+  return false;
+  }*/
+if (a==null || a=="")
+  {
+  alert("First name must be filled out");
+  return false;
+  }
+if (b==null || b=="")
+  {
+  alert("Last name must be filled out");
+  return false;
+  }
+if (c==null || c=="")
+  {
+  alert("Gender name must be filled out");
+  return false;
+  }
+if (d==null || d=="")
+  {
+  alert("address must be filled out");
+  return false;
+  }
+if (e==null || e=="")
+  {
+  alert("contact must be filled out");
+  return false;
+  }
+if (f==null || f=="")
+  {
+  alert("Du må lage et brukernavn");
+  return false;
+  }
+if (g==null || g=="")
+  {
+  alert("Du må lage et passord");
+  return false;
+  }
+if (h==null || h=="")
+  {
+  alert("Gjenta passord");
+  return false;
+  }
+if(g!=h){
+	
+	 alert("Passordet stemmer ikke overens");
+  return false;
+	}
+}
+</script>
 </head>
 
 <body>
@@ -46,16 +108,16 @@
     <td><input type="text" name="contact" /></td>
   </tr>
   <tr>
-    <td><div align="right">Bilede:</div></td>
-    <td><input type="text" name="pic" /></td>
-  </tr>
- <tr>
     <td><div align="right">Brukernavn:</div></td>
     <td><input type="text" name="username" /></td>
   </tr>
  <tr>
     <td><div align="right">Passord:</div></td>
     <td><input type="text" name="password" /></td>
+  </tr>
+ <tr>
+    <td><div align="right">Gjenta Passord:</div></td>
+    <td><input type="password" name="gpassword" /></td>
   </tr>
   <tr>
     <td><div align="right"></div></td>
