3492d9947c407988ac4fd1d9e262fdb69cc62c70
Behzad2256 <behzad@hotmail.no>
2013-10-09 14:24:51 +0200

    infodiv
    fått den til å oppdatere bilder fra en mappe automatisk og satt en
    intervall på det

--- www.markensgate.no/mg.js
+++ www.markensgate.no/mg.js
@@ -101,3 +101,51 @@
 	  });
   }
 
+
+//Kode for å laste in bilder inn i infodiveve
+
+
+
+
+
+
+
+function random_imglink(){
+var myimages=new Array();
+//specify random images below. You can have as many as you wish
+
+
+
+
+
+ // refresh every 10000 milliseconds
+
+
+myimages[1]="pic/infodiv/image1.gif";
+myimages[2]="pic/infodiv/image2.gif";
+myimages[3]="pic/infodiv/image3.gif";
+myimages[4]="pic/infodiv/image4.gif";
+myimages[5]="pic/infodiv/image5.gif";
+myimages[6]="pic/infodiv/image6.gif";
+
+	
+var ry=Math.floor(Math.random()*myimages.length);
+if (ry==0){
+ry=1;}
+
+
+document.getElementById("info1").innerHTML=('<img src="'+myimages[ry]+'" border=1 width="250px" height="250px" margin-bottom="5px">');
+$('#info1').fadeIn("slow");
+	
+
+
+}
+
+
+
+
+var auto_refresh = setInterval(
+function ()
+{
+random_imglink();
+}, 10000);
--- www.markensgate.no/registrering.php
+++ www.markensgate.no/registrering.php
@@ -10,66 +10,66 @@ var b=document.forms["reg"]["lname"].value;
 var c=document.forms["reg"]["mname"].value;
 var d=document.forms["reg"]["address"].value;
 var e=document.forms["reg"]["contact"].value;
-var f=document.forms["reg"]["username"].value;
-var g=document.forms["reg"]["password"].value;
-var h=document.forms["reg"]["gpassword"].value;
-/*if ((a==null || a=="") && (b==null || b=="") && (c==null || c=="") && (d==null || d=="") && (e==null || e=="") && (f==null || f==""))
+var f=document.forms["reg"]["pic"].value;
+var g=document.forms["reg"]["pic"].value;
+var h=document.forms["reg"]["pssword"].value;
+
+//if ((a==null || a=="") && (b==null || b=="") && (c==null || c=="") && (d==null || d=="") && (e==null || e=="") && (f==null || f==""))
   {
-  alert("All Field must be filled out");
+  alert("Alle felt må fylles ut");
   return false;
-  }*/
+  }
 if (a==null || a=="")
   {
-  alert("First name must be filled out");
+  alert("Alle felt må fylles ut");
   return false;
   }
 if (b==null || b=="")
   {
-  alert("Last name must be filled out");
+  alert("Alle felt må fylles ut");
   return false;
   }
 if (c==null || c=="")
   {
-  alert("Gender name must be filled out");
+  alert("Alle felt må fylles ut");
   return false;
   }
 if (d==null || d=="")
   {
-  alert("address must be filled out");
+  alert("Alle felt må fylles ut");
   return false;
   }
-if (e==null || e=="")
-  {
-  alert("contact must be filled out");
-  return false;
-  }
+//if (e==null || e=="")
+//  {
+//  alert("contact must be filled out");
+//  return false;
+//  }
 if (f==null || f=="")
   {
-  alert("Du må lage et brukernavn");
+  alert("Alle felt må fylles utt");
   return false;
   }
 if (g==null || g=="")
   {
-  alert("Du må lage et passord");
+  alert("Alle felt må fylles ut");
   return false;
   }
 if (h==null || h=="")
   {
-  alert("Gjenta passord");
+  alert("Alle felt må fylles ut");
   return false;
   }
-if(g!=h){
-	
-	 alert("Passordet stemmer ikke overens");
-  return false;
-	}
+  if(c!=h){
+	   alert("Passodet stemmer ikke overens");
+	  
+	  }
 }
 </script>
 </head>
 
 <body>
 
-<form name="reg" action="code_exec.php" onSubmit="return validateForm()" method="post">
+<form name="reg" action="code_exec.php" onsubmit="return validateForm()" method="post">
 <table width="274" border="0" align="center" cellpadding="2" cellspacing="0">
   <tr>
     <td colspan="2">
@@ -78,27 +78,24 @@ if(g!=h){
 		$remarks=$_GET['remarks'];
 		if ($remarks==null and $remarks=="")
 		{
-		echo 'Registrer her';
+		echo 'Register Here';
 		}
 		if ($remarks=='success')
 		{
-		echo 'Registrering var vellykket';
+		echo 'Registration Success';
 		}
 		?>	
 	    </div></td>
   </tr>
   <tr>
-    <td width="95"><div align="right">Fornavn:</div></td>
+    <td width="95"><div align="right">First Name:</div></td>
     <td width="171"><input type="text" name="fname" /></td>
   </tr>
   <tr>
-    <td><div align="right">Etternavn:</div></td>
+    <td><div align="right">Last Name:</div></td>
     <td><input type="text" name="lname" /></td>
   </tr>
-  <tr>
-    <td><div align="right">Kjønn:</div></td>
-    <td><input type="text" name="mname" /></td>
-  </tr>
+  
   <tr>
     <td><div align="right">Adresse:</div></td>
     <td><input type="text" name="address" /></td>
@@ -108,23 +105,26 @@ if(g!=h){
     <td><input type="text" name="contact" /></td>
   </tr>
   <tr>
+    <td><div align="right">E-mail:</div></td>
+    <td><input type="text" name="pic" /></td>
+  </tr>
+ <tr>
     <td><div align="right">Brukernavn:</div></td>
     <td><input type="text" name="username" /></td>
   </tr>
  <tr>
     <td><div align="right">Passord:</div></td>
-    <td><input type="text" name="password" /></td>
+    <td><input type="password" name="password" /></td>
   </tr>
- <tr>
-    <td><div align="right">Gjenta Passord:</div></td>
-    <td><input type="password" name="gpassword" /></td>
+  <tr>
+    <td><div align="right">Gjenta Passordet:</div></td>
+    <td><input type="password" name="mname" /></td>
   </tr>
   <tr>
     <td><div align="right"></div></td>
-    <td><input name="submit" type="submit" value="Registrer" /></td>
+    <td><input name="submit" type="submit" value="Submit" /></td>
   </tr>
 </table>
 </form>
-
 </body>
 </html>
